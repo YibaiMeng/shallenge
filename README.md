890aaa71e2d7d84f467157c95ec008844211f960
Yibai Meng <yibai@meng.engineer>
2024-06-21 01:05:14 -0700

    feat: use curand pseudorandom numbers instead of incrementing nounce
    This causes a slight regression (4.65 GH/s-> 4.55 GH/s) on my 3070Ti,
    but it makes state manangement easier.
    
    Also:
    
    - Change command line arguments. Takes in number of hashes (`--hashes`)
    to run instead number of kernel launches.
    - Add options `--block_size` and `--grid_size` to specify these parameters
    at runtime

--- README.md
+++ README.md
@@ -3,39 +3,42 @@
 
 Code for https://shallenge.quirino.net/ https://news.ycombinator.com/item?id=40683564
 
-To build:
-
-```
-nvcc -O3 -o shallenge shallenge.cu 
-```
+To build, run `make`.
 
 To run:
 ```
-$ ./shallenge --seed 0 --iter 100
-Starting seed: 0
-Iterations: 100
-Threads per launch: 49152
-Hashes in total: 5.15 TH
-Next seed is 5153960755200
-Hash rate: 4.7 GH / s
-Best nonce: bcigbepchaaaaaaa
-Best hash: 00000000 012b4fcf 7922ddb3 947d59a8 7673e1e1 fc1de9ce aba37511 dd81723d
-Iteration 1 of 100 completed.
-Elapsed time: 11.2 s
-Hash rate: 4.7 GH / s
-Iteration 2 of 100 completed.
-Elapsed time: 22.1 s
+$ ./build/shallenge --seed=1337 --hashes=0.1 --block_size=1024 --grid_size=48
+Number of devices: 1
+Device 0: NVIDIA GeForce RTX 3070 Ti
+  Compute capability: 8.6
+  Total global memory: 8589410304 bytes
+  Shared memory per block: 49152 bytes
+  Registers per block: 65536
+  Warp size: 32
+  Max threads per block: 1024
+  Max threads per multiprocessor: 1536
+  Number of multiprocessors: 48
+Register usage: 56
+Shared memory per block: 0 bytes
+Occupancy: 24576 threads per multiprocessor
+Seed: 1337
+Hashes in total: 0.100 TH
+Grid size 48, Block size: 1024, Threads: 49152
+Kernel launches: 2
+Hash rate: 5.05 GH / s
+Best nonce: melifkabmnmnjjda
+Best hash: 00000000 01a5ba71 c1fb13a2 02d8ad30 0784d9b7 ded85f69 02f5fd4f c70c53c6
+Iteration 1 of 2 completed.
+Elapsed time: 10.20 s
+Hash rate: 5.00 GH / s
+Iteration 2 of 2 completed.
+Elapsed time: 20.52 s
 ....
 
 ```
 A lot of stuff is hardcoded right now, including my username.
 
-To profile, compile the program with --lineinfo:
-
+To profile, compile the program with --lineinfo by running 
 ```
-nvcc -O3 -o shallenge shallenge.cu  --lineinfo
-```
-
-```
-ncu -o profile -f --import-source true --set full --target-processes=application-only -k regex:find_lowest_sha256  ./shallenge --seed 0 --iter 1
+make PROFILE=1 ncu
 ```
--- shallenge.cu
+++ shallenge.cu
@@ -7,12 +7,19 @@
 #include <iomanip>
 
 #include <cuda_runtime.h>
+#include <curand.h>
+#include <curand_kernel.h>
 #include <device_launch_parameters.h>
 #include "third_party/argparse/include/argparse/argparse.hpp"
 
+#include "cuda_device_property.cuh"
 #include "cuda_error.cuh"
 #include "sha256.cuh"
 #include "sha256_hash.h"
+
+constexpr int64_t iter_per_kernel = 1048576;
+#define USERNAME "yibaimeng/RTX3070Ti/mengyibai+dot+com/"
+constexpr int username_len = strlen(USERNAME);
 
 __device__ __host__ bool is_smaller(sha256_hash *a, sha256_hash *b)
 {
@@ -29,25 +36,38 @@ __device__ __host__ bool is_smaller(sha256_hash *a, sha256_hash *b)
     return false;
 }
 
-template <int32_t iter_per_kernel, int username_len>
-__global__ void find_lowest_sha256(int64_t *run_offset, int64_t *best_nonce, sha256_hash *best_hash)
+// curand state initialization is done in a different kernel because its quite time consuming when subsequence is larger.
+__global__ void init_curand_state(uint64_t seed, curandStateXORWOW_t *state)
+{
+    uint64_t tid = blockDim.x * blockIdx.x + threadIdx.x;
+    curand_init(seed, tid, 0, state + tid);
+}
+
+template <int username_len>
+__global__ void find_lowest_sha256(uint64_t *best_nonce, sha256_hash *best_hash, int32_t iter_per_kernel, curandStateXORWOW_t *curand_state)
 {
     sha256_hash thread_best_hash;
     memset(reinterpret_cast<void *>(thread_best_hash.hash), 0xff, 32); // Initialize with high values
+    static_assert(strlen(USERNAME) == username_len);
+    static_assert(username_len + 16 < 62); // That's the most a single sha256 block takes?
 
-    char buffer[64] = "yibaimeng/";
+    char buffer[64] = USERNAME;
     sha256_hash hash;
     int32_t t_id = blockDim.x * blockIdx.x + threadIdx.x;
-    int64_t nounce = run_offset[0] + (int64_t)t_id * (int64_t)iter_per_kernel;
     int64_t thread_best_nounce = 0;
+
+    curandState local_curand_state = curand_state[t_id];
+
     for (int c = 0; c < iter_per_kernel; c++)
     {
-        nounce++;
+        uint32_t r1 = curand(&local_curand_state);
+        uint32_t r2 = curand(&local_curand_state);
+        uint64_t nounce = ((uint64_t)r1 << 32) + (uint64_t)r2;
 
 #pragma unroll
         for (int i = 0; i < 64; i += 4)
         {
-            buffer[username_len + i / 4] = ('a' + (int)((int64_t)(nounce >> i) & 0xf));
+            buffer[username_len + i / 4] = ('a' + ((nounce >> i) & 0xf));
         }
 
         constexpr int len = username_len + 16;
@@ -69,23 +89,19 @@ __global__ void find_lowest_sha256(int64_t *run_offset, int64_t *best_nonce, sha
 
     best_nonce[t_id] = thread_best_nounce;
     best_hash[t_id] = thread_best_hash;
+    curand_state[t_id] = local_curand_state;
 }
 
-std::string nounce_to_string(int64_t nounce)
+std::string nounce_to_string(uint64_t nounce)
 {
     char buffer[20];
     for (int i = 0; i < 64; i += 4)
     {
-        buffer[i / 4] = ('a' + (int)((int64_t)(nounce >> i) & 0xf));
+        buffer[i / 4] = ('a' + (int)((uint64_t)(nounce >> i) & 0xf));
     }
     buffer[16] = 0;
     return std::string(buffer);
 }
-
-constexpr int grid_size = 48;
-constexpr int block_size = 1024;
-constexpr int64_t num_threads_per_launch = grid_size * block_size;
-constexpr int64_t iter_per_thread = 1048576;
 
 int main(int argc, char *argv[0])
 {
@@ -96,10 +112,22 @@ int main(int argc, char *argv[0])
         .required()
         .scan<'i', int64_t>();
 
-    program.add_argument("--iter")
-        .help("set iteration value")
-        .required()
-        .scan<'i', int64_t>();
+    program.add_argument("--hashes")
+        .help("amount of hashes to compute, in TH.")
+        .default_value(1.0)
+        .scan<'g', double>();
+    program.add_argument("--grid_size")
+        .help("grid size for the kernel launch")
+        .default_value(48)
+        .scan<'i', int>();
+    program.add_argument("--block_size")
+        .help("block size for the kernel launch")
+        .default_value(1024)
+        .scan<'i', int>();
+    program.add_argument("--dry-run")
+        .help("don't run, only show device property and run configurations")
+        .default_value(false)
+        .implicit_value(true);
 
     try
     {
@@ -113,49 +141,58 @@ int main(int argc, char *argv[0])
     }
 
     int64_t cmd_seed = program.get<int64_t>("--seed");
-    int64_t cmd_iter = program.get<int64_t>("--iter");
+    double cmd_hash = program.get<double>("--hashes");
+    int grid_size = program.get<int>("--grid_size");
+    int block_size = program.get<int>("--block_size");
+    int64_t num_threads_per_launch = grid_size * block_size;
+    double hashes_per_kernel = cmd_hash * 1e12 / static_cast<double>(num_threads_per_launch);
+    int64_t cmd_iter = static_cast<int64_t>(std::ceil(hashes_per_kernel / iter_per_kernel));
 
-    std::cout << "Starting seed: " << cmd_seed << std::endl;
-    std::cout << "Iterations: " << cmd_iter << std::endl;
-    std::cout << "Threads per launch: " << num_threads_per_launch << std::endl;
-    std::cout << std::fixed << std::setprecision(2) << "Hashes in total: " << (double)num_threads_per_launch * (double)cmd_iter * (double)iter_per_thread / 1e12 << " TH" << std::endl;
-    int64_t next_seed = cmd_seed + (int64_t)num_threads_per_launch * (int64_t)cmd_iter * (int64_t)iter_per_thread;
-    if (next_seed < cmd_seed)
+    print_cuda_device_property();
+    print_kernel_attributes(find_lowest_sha256<username_len>);
+
+    std::cout << "Seed: " << cmd_seed << std::endl;
+    std::cout << std::fixed << std::setprecision(3) << "Hashes in total: " << cmd_hash << " TH" << std::endl;
+    std::cout << "Grid size " << grid_size << ", Block size: " << block_size << ", Threads: " << num_threads_per_launch << std::endl;
+    std::cout << "Kernel launches: " << cmd_iter << std::endl;
+    if (program.get<bool>("--dry-run"))
     {
-        std::cout << "Overflows! no next seed" << std::endl;
+        exit(0);
     }
-    std::cout << "Next seed is " << next_seed << std::endl;
+
     auto sys_start = std::chrono::high_resolution_clock::now();
 
     cudaEvent_t start, stop;
     CUDA_CHECK(cudaEventCreate(&start));
     CUDA_CHECK(cudaEventCreate(&stop));
 
-    int64_t *d_best_nonce;
+    uint64_t *d_best_nonce;
     sha256_hash *d_best_hash;
-    int64_t *d_run_offset;
-    CUDA_CHECK(cudaMalloc(&d_run_offset, 1 * sizeof(int64_t)));
-    CUDA_CHECK(cudaMalloc(&d_best_nonce, num_threads_per_launch * sizeof(int64_t)));
-    CUDA_CHECK(cudaMalloc(&d_best_hash, num_threads_per_launch * sizeof(sha256_hash)));
-    int64_t *h_best_nonce = new int64_t[num_threads_per_launch];
+    curandStateXORWOW_t *curand_state;
+
+    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_nonce), num_threads_per_launch * sizeof(uint64_t)));
+    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_hash), num_threads_per_launch * sizeof(sha256_hash)));
+    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&curand_state), num_threads_per_launch * sizeof(curandStateXORWOW_t)));
+    uint64_t *h_best_nonce = new uint64_t[num_threads_per_launch];
     sha256_hash *h_best_hash = new sha256_hash[num_threads_per_launch];
 
     sha256_hash program_best_hash;
     memset(reinterpret_cast<void *>(program_best_hash.hash), 0xff, 32); // Initialize with high values
 
+    init_curand_state<<<grid_size, block_size>>>(cmd_seed, curand_state);
+    CUDA_CHECK(cudaDeviceSynchronize());
+
     for (int64_t iter = 0; iter < cmd_iter; iter++)
     {
-        int64_t nounce_offset = cmd_seed + (int64_t)iter * ((int64_t)num_threads_per_launch * (int64_t)iter_per_thread);
-        CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(d_run_offset), reinterpret_cast<void *>(&nounce_offset), 1 * sizeof(int64_t), cudaMemcpyHostToDevice));
         CUDA_CHECK(cudaEventRecord(start, 0));
-        find_lowest_sha256<iter_per_thread, 10><<<grid_size, block_size>>>(d_run_offset, d_best_nonce, d_best_hash);
+        find_lowest_sha256<username_len><<<grid_size, block_size>>>(d_best_nonce, d_best_hash, iter_per_kernel, curand_state);
         CUDA_CHECK(cudaEventRecord(stop, 0));
-        CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(h_best_nonce), reinterpret_cast<void *>(d_best_nonce), num_threads_per_launch * sizeof(int64_t), cudaMemcpyDeviceToHost));
+        CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(h_best_nonce), reinterpret_cast<void *>(d_best_nonce), num_threads_per_launch * sizeof(uint64_t), cudaMemcpyDeviceToHost));
         CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(h_best_hash), reinterpret_cast<void *>(d_best_hash), num_threads_per_launch * sizeof(sha256_hash), cudaMemcpyDeviceToHost));
 
         sha256_hash iter_best_hash;
         memset(reinterpret_cast<void *>(iter_best_hash.hash), 0xff, 32); // Initialize with high values
-        int64_t iter_best_nounce = -1;
+        uint64_t iter_best_nounce = 0;
         for (int i = 0; i < num_threads_per_launch; i++)
         {
             if (is_smaller(h_best_hash + i, &iter_best_hash))
@@ -169,7 +206,7 @@ int main(int argc, char *argv[0])
         float elapsedTime;
         CUDA_CHECK(cudaEventElapsedTime(&elapsedTime, start, stop));
 
-        std::cout << std::fixed << std::setprecision(1) << "Hash rate: " << (double)(num_threads_per_launch * iter_per_thread) / (double)elapsedTime / 1e6 << " GH / s" << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << "Hash rate: " << (double)(num_threads_per_launch * iter_per_kernel) / (double)elapsedTime / 1e6 << " GH / s" << std::endl;
         if (is_smaller(&iter_best_hash, &program_best_hash))
         {
             std::cout << "Best nonce: " << nounce_to_string(iter_best_nounce) << std::endl;
