86c8bde90f3be6e53b54e243f334cd87dac7273b
Yibai Meng <yibai@meng.engineer>
2024-06-22 14:50:44 -0700

    feat: allow using multiple GPUs for hashing
    Add `--num_gpus` flag. The multiple GPUs will run the kernels in parallel.
    
    The host side is still happening in one thread though, so there will be
    some reduction in per-GPU hash rate.

--- src/shallenge.cu
+++ src/shallenge.cu
@@ -116,6 +116,11 @@ int main(int argc, char *argv[0])
         .help("amount of hashes to compute, in TH.")
         .default_value(1.0)
         .scan<'g', double>();
+    program.add_argument("--num_gpus")
+        .help("number of GPUs to run the program on")
+        .default_value(1)
+        .scan<'i', int>();
+
     program.add_argument("--grid_size")
         .help("grid size for the kernel launch")
         .default_value(48)
@@ -140,16 +145,24 @@ int main(int argc, char *argv[0])
         std::exit(1);
     }
 
+    int num_gpus = program.get<int>("--num_gpus");
     int64_t cmd_seed = program.get<int64_t>("--seed");
     double cmd_hash = program.get<double>("--hashes");
     int grid_size = program.get<int>("--grid_size");
     int block_size = program.get<int>("--block_size");
     int64_t num_threads_per_launch = grid_size * block_size;
     double hashes_per_kernel = cmd_hash * 1e12 / static_cast<double>(num_threads_per_launch);
-    int64_t cmd_iter = static_cast<int64_t>(std::ceil(hashes_per_kernel / iter_per_kernel));
+    int64_t cmd_iter = static_cast<int64_t>(std::ceil(hashes_per_kernel * num_gpus / iter_per_kernel));
 
     print_cuda_device_property();
     print_kernel_attributes(find_lowest_sha256<username_len>);
+    int device_count = 0;
+    CUDA_CHECK(cudaGetDeviceCount(&device_count));
+    if (device_count > num_gpus)
+    {
+        std::cerr << "Number of devices smaller than what's specified in --num_gpus" << std::endl;
+        exit(1);
+    }
 
     std::cerr << "Seed: " << cmd_seed << std::endl;
     std::cerr << std::fixed << std::setprecision(3) << "Hashes in total: " << cmd_hash << " TH" << std::endl;
@@ -162,51 +175,76 @@ int main(int argc, char *argv[0])
 
     auto sys_start = std::chrono::high_resolution_clock::now();
 
-    cudaEvent_t start, stop;
-    CUDA_CHECK(cudaEventCreate(&start));
-    CUDA_CHECK(cudaEventCreate(&stop));
+    std::vector<cudaEvent_t> start_events(num_gpus), stop_events(num_gpus);
+    std::vector<cudaStream_t> streams(num_gpus);
+    std::vector<uint64_t *> d_best_nonce(num_gpus);
+    std::vector<sha256_hash *> d_best_hash(num_gpus);
+    std::vector<curandStateXORWOW_t *> curand_state(num_gpus);
+    std::vector<uint64_t *> h_best_nonce(num_gpus);
+    std::vector<sha256_hash *> h_best_hash(num_gpus);
 
-    uint64_t *d_best_nonce;
-    sha256_hash *d_best_hash;
-    curandStateXORWOW_t *curand_state;
+    for (int gpu_id = 0; gpu_id < num_gpus; ++gpu_id)
+    {
+        CUDA_CHECK(cudaSetDevice(gpu_id));
+        CUDA_CHECK(cudaStreamCreate(&streams[gpu_id]));
+        cudaEvent_t &start = start_events[gpu_id];
+        cudaEvent_t &stop = stop_events[gpu_id];
+        CUDA_CHECK(cudaEventCreate(&start));
+        CUDA_CHECK(cudaEventCreate(&stop));
 
-    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_nonce), num_threads_per_launch * sizeof(uint64_t)));
-    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_hash), num_threads_per_launch * sizeof(sha256_hash)));
-    CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&curand_state), num_threads_per_launch * sizeof(curandStateXORWOW_t)));
-    uint64_t *h_best_nonce = new uint64_t[num_threads_per_launch];
-    sha256_hash *h_best_hash = new sha256_hash[num_threads_per_launch];
+        CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_nonce[gpu_id]), num_threads_per_launch * sizeof(uint64_t)));
+        CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&d_best_hash[gpu_id]), num_threads_per_launch * sizeof(sha256_hash)));
+        CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&curand_state[gpu_id]), num_threads_per_launch * sizeof(curandStateXORWOW_t)));
+        h_best_nonce[gpu_id] = new uint64_t[num_threads_per_launch];
+        h_best_hash[gpu_id] = new sha256_hash[num_threads_per_launch];
+
+        init_curand_state<<<grid_size, block_size, 0, streams[gpu_id]>>>(cmd_seed + gpu_id, curand_state[gpu_id]);
+    }
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     sha256_hash program_best_hash;
     memset(reinterpret_cast<void *>(program_best_hash.hash), 0xff, 32); // Initialize with high values
-
-    init_curand_state<<<grid_size, block_size>>>(cmd_seed, curand_state);
-    CUDA_CHECK(cudaDeviceSynchronize());
-
     for (int64_t iter = 0; iter < cmd_iter; iter++)
     {
-        CUDA_CHECK(cudaEventRecord(start, 0));
-        find_lowest_sha256<username_len><<<grid_size, block_size>>>(d_best_nonce, d_best_hash, iter_per_kernel, curand_state);
-        CUDA_CHECK(cudaEventRecord(stop, 0));
-        CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(h_best_nonce), reinterpret_cast<void *>(d_best_nonce), num_threads_per_launch * sizeof(uint64_t), cudaMemcpyDeviceToHost));
-        CUDA_CHECK(cudaMemcpy(reinterpret_cast<void *>(h_best_hash), reinterpret_cast<void *>(d_best_hash), num_threads_per_launch * sizeof(sha256_hash), cudaMemcpyDeviceToHost));
+        for (int gpu_id = 0; gpu_id < num_gpus; ++gpu_id)
+        {
+            CUDA_CHECK(cudaSetDevice(gpu_id));
+            CUDA_CHECK(cudaEventRecord(start_events[gpu_id], streams[gpu_id]));
+            find_lowest_sha256<username_len><<<grid_size, block_size, 0, streams[gpu_id]>>>(d_best_nonce[gpu_id], d_best_hash[gpu_id], iter_per_kernel, curand_state[gpu_id]);
+            CUDA_CHECK(cudaEventRecord(stop_events[gpu_id], streams[gpu_id]));
+        }
+
+        for (int gpu_id = 0; gpu_id < num_gpus; ++gpu_id)
+        {
+            CUDA_CHECK(cudaSetDevice(gpu_id));
+            CUDA_CHECK(cudaMemcpyAsync(reinterpret_cast<void *>(h_best_nonce[gpu_id]), reinterpret_cast<void *>(d_best_nonce[gpu_id]), num_threads_per_launch * sizeof(uint64_t), cudaMemcpyDeviceToHost, streams[gpu_id]));
+            CUDA_CHECK(cudaMemcpyAsync(reinterpret_cast<void *>(h_best_hash[gpu_id]), reinterpret_cast<void *>(d_best_hash[gpu_id]), num_threads_per_launch * sizeof(sha256_hash), cudaMemcpyDeviceToHost, streams[gpu_id]));
+        }
 
         sha256_hash iter_best_hash;
         memset(reinterpret_cast<void *>(iter_best_hash.hash), 0xff, 32); // Initialize with high values
         uint64_t iter_best_nounce = 0;
-        for (int i = 0; i < num_threads_per_launch; i++)
+        float total_elasped_time = 0;
+        for (int gpu_id = 0; gpu_id < num_gpus; ++gpu_id)
         {
-            if (is_smaller(h_best_hash + i, &iter_best_hash))
+            CUDA_CHECK(cudaSetDevice(gpu_id));
+            CUDA_CHECK(cudaStreamSynchronize(streams[gpu_id]));
+            CUDA_CHECK(cudaEventSynchronize(stop_events[gpu_id]));
+            float elapsed_time;
+            CUDA_CHECK(cudaEventElapsedTime(&elapsed_time, start_events[gpu_id], stop_events[gpu_id]));
+            std::cerr << "GPU " << gpu_id << " spent " << elapsed_time << " ms this iteration" << std::endl;
+            total_elasped_time += elapsed_time;
+            for (int i = 0; i < num_threads_per_launch; i++)
             {
-                iter_best_hash = h_best_hash[i];
-                iter_best_nounce = h_best_nonce[i];
+                if (is_smaller(h_best_hash[gpu_id] + i, &iter_best_hash))
+                {
+                    iter_best_hash = h_best_hash[gpu_id][i];
+                    iter_best_nounce = h_best_nonce[gpu_id][i];
+                }
             }
         }
 
-        CUDA_CHECK(cudaEventSynchronize(stop));
-        float elapsedTime;
-        CUDA_CHECK(cudaEventElapsedTime(&elapsedTime, start, stop));
-
-        std::cerr << std::fixed << std::setprecision(2) << "Hash rate: " << (double)(num_threads_per_launch * iter_per_kernel) / (double)elapsedTime / 1e6 << " GH / s" << std::endl;
+        std::cerr << std::fixed << std::setprecision(2) << "Hash rate per GPU: " << (double)(num_threads_per_launch * iter_per_kernel * num_gpus) / (double)total_elasped_time / 1e6 << " GH / s" << std::endl;
         if (is_smaller(&iter_best_hash, &program_best_hash))
         {
             std::cerr << "Best nonce: " << nounce_to_string(iter_best_nounce) << std::endl;
@@ -216,10 +254,16 @@ int main(int argc, char *argv[0])
         std::cerr << "Iteration " << iter + 1 << " of " << cmd_iter << " completed. " << std::endl;
         std::cerr << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - sys_start).count() / 1000.0 << " s" << std::endl;
     }
-    CUDA_CHECK(cudaFree(d_best_nonce));
-    CUDA_CHECK(cudaFree(d_best_hash));
-    delete[] h_best_hash;
-    delete[] h_best_nonce;
+
+    for (int gpu_id = 0; gpu_id < num_gpus; ++gpu_id)
+    {
+        CUDA_CHECK(cudaSetDevice(gpu_id));
+        CUDA_CHECK(cudaFree(d_best_nonce[gpu_id]));
+        CUDA_CHECK(cudaFree(d_best_hash[gpu_id]));
+        delete[] h_best_hash[gpu_id];
+        delete[] h_best_nonce[gpu_id];
+        CUDA_CHECK(cudaStreamDestroy(streams[gpu_id]));
+    }
 
     return 0;
 }
